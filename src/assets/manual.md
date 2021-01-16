2f1a7a7c97bdb3cca8fa4ed07bfa2b245c2c1232
paulopacitti <ppacitti@outlook.com>
2021-01-16 16:00:43 -0300

    feat: improved CLI experience

--- src/assets/manual.md
+++
@@ -1,6 +0,0 @@
-# This is an h1
-
-Rich can do a pretty *decent* job of rendering markdown.
-
-1. This is a list item
-2. This is another list item
--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
-import sys, os, signal
+import sys
+import os
+import signal
+from rich.console import Console
+from rich.panel import Panel
+from rich.prompt import IntPrompt
+
 # Disable print
 def blockPrint():
     sys.stdout = open(os.devnull, 'w')
@@ -10,46 +16,47 @@ def enablePrint():
 blockPrint()
 import pygame.mixer as mixer
 enablePrint()
-
-from rich.console import Console
-from rich.markdown import Markdown
-from rich.panel import Panel
-from rich.prompt import IntPrompt
-
-def init():
-    global channel1
-    global console
-    global manual
-
-    mixer.init()
-    channel1 = mixer.Channel(0)
-    console = Console()
     
-    with open("assets/manual.md") as raw:
-        manual = Markdown(raw.read())
-
 def main():
     console.clear()
     rain()
 
-    console.print(Panel("Welcome to the [red]Onsen!", expand=True))
-    console.print(manual)
+    console.print(Panel("[bold]Welcome to [default on red]onsen[/]![/] ([italic]tattoo-friendly)[/]", expand=True))
+    console.print("[italic]May I help you? (いらっしゃいませ)[/] type [default on red]help[/]")
     while True:
         command = console.input(":hotsprings:  [bold red]>[/] ")
-        if command == 'quit':
-            break
-        if command == 'rain':
-            console.print("[blue]Pouring rain...")
+       
+        if command == 'bath':
+            console.print("[blue]Relax...")
             rain()
-        if command == 'stop':
-            stop()
         if command == 'volume':
             value = IntPrompt.ask(":cloud_with_rain:  [bold blue]How much water do you want? (1 to 100)[/]")
             set_volume(value)
+        if command == "help":
+            console.print(help_commands)
+        if command == 'stop':
+            stop()
+        if command == 'quit':
+            console.print("[italic]Come back soon![/] (ありがとうございました)")
+            break
+        
     return
 
+def init():
+    global channel1
+    global console
+    global help_commands
+
+    mixer.init()
+    channel1 = mixer.Channel(0)
+    console = Console()
+    help_commands = """[default on red]bath[/] - take a bath in our hot springs :umbrella: 
+[default on red]volume[/] - turn down [strike]for what[/] :arrow_up_down:
+[default on red]stop[/] - shut [bold]everything[/] up :speaker:
+[default on red]quit[/] - I [italic]think[/] you know what this does :x:"""
+
 def rain():
-    rain = mixer.Sound('assets/rain.wav')
+    rain = mixer.Sound('assets/water.wav')
     channel1.play(rain, loops = -1)
 
 def stop():
@@ -62,8 +69,9 @@ def set_volume(value):
 ## handle CTRL + C
 def signal_handler(signal, frame):
     print()
+    console.print("[italic]Come back soon![/] (ありがとうございました)")
     sys.exit(0)
 
+signal.signal(signal.SIGINT, signal_handler)
 init()
 main()
-signal.signal(signal.SIGINT, signal_handler)
