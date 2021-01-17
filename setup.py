f86e99accd2434fb4fa7830d452ba2bb29c7b5db
paulopacitti <ppacitti@outlook.com>
2021-01-17 18:13:20 -0300

    release: prepared for release in PyPI

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='onsen',
+    packages=['onsen'],
+    version='0.1.3',
+    license='MIT',
+    author='Paulo Pacitti',
+    author_email='ppacitti@outlook.com',
+    url='https://github.com/paulopacitti/onsen',
+    description='rain sounds straight to your terminal!',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={'console_scripts': ['onsen= onsen.__main__:main']},
+    include_package_data=True,
+    install_requires=['pygame', 'rich'],
+    keywords=['rain', 'cli', 'onsen', 'rain', 'focus', 'water', 'sounds', 'white noise'],
+)
